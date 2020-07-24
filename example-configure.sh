a88ae3c9b21514fa871f34463d11daa381f732e2
chu <chuweiqi@sun-tech.cn>
2020-07-24 14:39:01 +0800

    添加全局

--- example-configure.sh
+++ example-configure.sh
@@ -4,14 +4,14 @@ LOCATION=${1:-`date "+%F-%T"`}
 
 echo -en "\033[41m""save location is \"${LOCATION}\"""\033[49m"
 
-[ ! -f ./backup ] && mkdir backup
+#[ ! -f ./backup ] && mkdir backup
 
-ansible -i hosts -m setup --tree=/tmp/host_data 
+ansible -i hosts all -m setup --tree=/tmp/host_data/
 
 ll /usr/bin/ansible-cmdb
 if [ $? = 0 ]; then
-    ansible-cmdb /tmp/host_data > ./host_data_${LOCATION}.html  
+    ansible-cmdb /tmp/host_data/ > ./host_data_${LOCATION}.html  
 else
     pip install ansible-cmdb
-    ansible-cmdb /tmp/host_data > ./host_data_${LOCATION}.html  
+    ansible-cmdb /tmp/host_data/ > ./host_data_${LOCATION}.html  
 fi 
